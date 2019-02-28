5998a84730c06d5063bc30f66524b16f71e63091
xiehang <850525344@qq.com>
2019-02-28 15:18:01 +0800

    暂存master

---
+++ build/precache-manifest.05f92caf25888fbbda519fc902e3b8e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b38ed25fc836f3aa12b6",
+    "url": "/static/js/main.b38ed25f.chunk.js"
+  },
+  {
+    "revision": "414842182579af040c62",
+    "url": "/static/js/1.41484218.chunk.js"
+  },
+  {
+    "revision": "b38ed25fc836f3aa12b6",
+    "url": "/static/css/main.d6d51b7c.chunk.css"
+  },
+  {
+    "revision": "1ab89bf9849599beec99deee26172918",
+    "url": "/index.html"
+  }
+];
