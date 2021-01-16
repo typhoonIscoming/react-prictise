3d3750efb68345ace9bbefed327e19a1d91212ff
xiehang <xiehang@encdata.cn>
2021-01-17 01:24:19 +0800

    函数式

--- src/pages/Find.js
+++ src/pages/Find.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux'
 // import { map } from 'lodash'
 
@@ -9,68 +9,36 @@ import '../css/find.scss'
 
 import { getLaugh, getHistory } from '../model/action'
 
-// const ShowElement = (props) => {
-//     const { status, result, error } = props
-//     if(status === 0) {
-//         return ( <Icon type="loading" /> )
-//     } else if(status === 1) {
-//         return map(result, (item, index) => {
-//             return (<div className="content-item" key={ index }>
-//                 <p>{ item.title }</p>
-//                 <img src={ item.img }  alt={ item.title } />
-//             </div>)
-//         })
-//     } else if(status === 2) {
-//         return (
-//             <div>{ error }</div>
-//         )
-//     } else {
-//         return (<div></div>)
-//     }
-// }
+const renderContent = (tab, index) => {
+    return (
+        <div className="result-container" key={index}>
+            <p>Content of {tab.title}</p>
+        </div>
+    );
+}
 
+function createMarkup() {
+    return {__html: 'First &middot; Second'};
+}
 
-class Find extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: 'zhangsan',
-            age: 10,
-            numberOfGuests: 3,
-            tabs: [{ title: '笑话大全'}, { title: '历史上的今天' }, { title: '生僻字' }, { title: '24点' }],
-        }
-    }
-    getData(index) {
-        if(index === 0) this.props.getLaugh()
-        else if(index === 1) this.props.getHistory()
-    }
-    skip() {
-        console.log(this.props)
-        this.props.history.push({ pathname: '/find/subFind' })
-    }
-    renderContent = tab => {
-        return (
-            <div className="result-container">
-                <p>Content of {tab.title}</p>
+function Find(props) {
+    const tabs = [{ title: '笑话大全'}, { title: '历史上的今天' }, { title: '生僻字' }, { title: '24点' }]
+    return (
+        <div className="find-page">
+            <div className='find-container'>
+                <Tabs
+                    tabs={ tabs }
+                    renderTabBar={ prop => <Tabs.DefaultTabBar {...prop} page={3} /> }
+                >
+                    {
+                        tabs.map((item, index) => renderContent(item, index))
+                    }
+                </Tabs>
+                <div dangerouslySetInnerHTML={{__html: 'First &middot; Second'}} />
             </div>
-        );
-    }
-    render() {
-        const { tabs } = this.state
-        return (
-            <div className="find-page">
-                <div className='find-container'>
-                    <Tabs
-                        tabs={ tabs }
-                        renderTabBar={ props => <Tabs.DefaultTabBar {...props} page={3} /> }
-                    >
-                        { this.renderContent }
-                    </Tabs>
-                </div>
-                <Menu />
-            </div>
-        )
-    }
+            <Menu />
+        </div>
+    )
 
 }
 
